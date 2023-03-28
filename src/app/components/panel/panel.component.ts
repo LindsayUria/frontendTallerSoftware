8b5552db7c51ed628904264ec414b314ef4a357c
Jose Pozo Silva <jhosede23@gmail.com>
2023-03-28 09:59:09 -0400

    updating styles

--- src/app/components/panel/panel.component.ts
+++ src/app/components/panel/panel.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface Solicitud {
+  numero: number;
+  nombre: string;
+  asunto: string;
+  fecha: Date;
+  hora: string;
+  estado: 'rechazado' | 'aprobado' | 'pendiente';
+}
+
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
@@ -7,10 +16,39 @@ import { Component } from '@angular/core';
 })
 export class PanelComponent {
 
-  solicitudes = [
-    { id: 1, nombre: 'Solicitud 1', descripcion: 'Descripción de la solicitud 1', estado: 'Pendiente' },
-    { id: 2, nombre: 'Solicitud 2', descripcion: 'Descripción de la solicitud 2', estado: 'Aprobada' },
-    { id: 3, nombre: 'Solicitud 3', descripcion: 'Descripción de la solicitud 3', estado: 'Rechazada' }
+  solicitudes: Solicitud[] = [
+    {
+      numero: 1,
+      nombre: 'Juan Pérez',
+      asunto: 'Solicitud de permiso',
+      fecha: new Date(2023, 2, 27),
+      hora: '14:30',
+      estado: 'pendiente'
+    },
+    {
+      numero: 2,
+      nombre: 'María Gómez',
+      asunto: 'Solicitud de vacaciones',
+      fecha: new Date(2023, 2, 28),
+      hora: '10:00',
+      estado: 'aprobado'
+    },
+    {
+      numero: 3,
+      nombre: 'Pedro Rodríguez',
+      asunto: 'Solicitud de presupuesto',
+      fecha: new Date(2023, 2, 28),
+      hora: '16:15',
+      estado: 'rechazado'
+    }
   ];
+
+  cambiarEstado(solicitud: Solicitud, nuevoEstado: 'rechazado' | 'aprobado' | 'pendiente') {
+    solicitud.estado = nuevoEstado;
+  }
+
+  obtenerFecha(fecha: Date): string {
+    return fecha.toLocaleDateString();
+  }
 
 }
