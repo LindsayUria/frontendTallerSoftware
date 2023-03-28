a1698f0bbf00d8f8895bf571af25ffaa45992a4b
Raul Alejandro Choque Valda <raul.choque@ucb.edu.bo>
2023-03-27 21:40:03 -0400

    Component for Add Posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AddPostsComponent } from './components/add-posts/add-posts.component';
+const routes: Routes = [
+  {
+    path: 'add-posts',
+    component: AddPostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
