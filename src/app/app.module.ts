f8bb5417027f225352c21be96e7e1f1740a6caef
Jose Pozo Silva <jhosede23@gmail.com>
2023-04-06 16:28:25 -0400

    fixing errors of libraries on module ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,12 @@ import { PanelComponent } from './components/panel/panel.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-<<<<<<< HEAD
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 
 
 
-=======
 import { AddEventsComponent } from './components/add-events/add-events.component';
->>>>>>> 13a6324e1d3dde334fb5cc84957366f12973a37c
 
 @NgModule({
   declarations: [
@@ -28,12 +25,9 @@ import { AddEventsComponent } from './components/add-events/add-events.component
     InicioComponent,
     NavbarComponent,
     FooterComponent,
-<<<<<<< HEAD
 
 
-=======
     AddEventsComponent,
->>>>>>> 13a6324e1d3dde334fb5cc84957366f12973a37c
 
   ],
   imports: [
@@ -41,15 +35,10 @@ import { AddEventsComponent } from './components/add-events/add-events.component
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-<<<<<<< HEAD
     AppRoutingModule,
     MatButtonModule,
-    MatDialogModule
-
-
-=======
+    MatDialogModule,
     AppRoutingModule
->>>>>>> 13a6324e1d3dde334fb5cc84957366f12973a37c
   ],
 
   providers: [],
