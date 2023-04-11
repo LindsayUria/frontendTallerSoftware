bb5b9c0ab4dcc8f3af75c5e2ed26f56fb9cccda0
Raul Alejandro Choque Valda <raul.choque@ucb.edu.bo>
2023-04-11 19:10:01 -0400

    Corrigiendo Observaciones

--- src/app/components/add-events/add-events.component.ts
+++ src/app/components/add-events/add-events.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-events',
   templateUrl: './add-events.component.html',
   styleUrls: ['./add-events.component.css']
 })
-export class AddEventsComponent {
+export class AddEventsComponent implements OnInit {
+  selectedModality: string= "";
+  selectedScope: string = "";
 
+  constructor(){}
+  ngOnInit(): void {
+
+  }
 }
