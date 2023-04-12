68e7e456628ae25fef2036ae7e47181e133e98a1
Dubuntu <josignaciol89@gmail.com>
2023-04-12 17:31:08 -0400

    union

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http:HttpClient) {
+    console.log('Servicio HTTP:');
+   }
+
+   GetSolicitud(){
+    return this.http.get('http://localhost:8080/solicitud/test');
+   }
+}
